76a914b658decab5c269b10cf01d07b316ca0f16
Robb Knight <desk@rknight.me>
2020-07-20 19:42:00 +0100

    Fix passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     passthroughFileCopy: true,
