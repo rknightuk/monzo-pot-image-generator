535efe28862b68cb943a36a7e30ef7d8f0cab6d1
Robb Knight <desk@rknight.me>
2020-07-20 22:50:08 +0100

    Add all this gubbins

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/mstile-150x150.png");
+  eleventyConfig.addPassthroughCopy("src/safari-pinned-tab.svg");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 
   return {
     passthroughFileCopy: true,
