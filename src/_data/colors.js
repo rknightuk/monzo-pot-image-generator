435c19d44a9cf3d763a12ce4342087a246630f1c
Robb Knight <desk@rknight.me>
2020-07-21 11:14:24 +0100

    make hot coral the default and one of the options

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -1,5 +1,6 @@
 module.exports = {
 	background: [
+		"#ed3b56",
 		"#EC5B5C",
 		"#e67e22",
 		"#f1c40f",
--- src/_data/config.js
+++ src/_data/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: "Monzo Pot Image Generator",
 	description: "Generate images for your Monzo pots",
-	primary: "#EC5B5C",
+	primary: "#ed3b56",
 	dark: "#333333",
 	light: "#f7f7f7",
 	icon: {
