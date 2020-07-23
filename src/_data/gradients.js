9f0a5f899e3b55ed04546314db848f00efa2372f
Robb Knight <desk@rknight.me>
2020-07-23 17:02:59 +0100

    Apply gradients

---
+++ src/_data/gradients.js
@@ -0,0 +1,44 @@
+module.exports = [
+	{
+		name: "blue",
+		left: "#1F3DF0",
+		right: "#3ABEDC"
+	},
+	{
+		name: "bluegreen",
+		left: "#1FB6DC",
+		right: "#3AD983"
+	},
+	{
+		name: "bluepurple",
+		left: "#982DF2",
+		right: "#5572E7"
+	},
+	{
+		name: "green",
+		left: "#3AF01E",
+		right: "#BFDC3A"
+	},
+	{
+		name: "orange",
+		left: "#F29920",
+		right: "#E7553A",
+	},
+	{
+		name: "purple",
+		left: "#DD20B6",
+		right: "#833AD9",
+	},
+	{
+		name: "redpink",
+		left: "#E8203B",
+		right: "#B73ABE"
+	},
+	{
+		name: "yelloworange",
+		left: "#F2D821",
+		right: "#E7813A"
+	}
+]
+
+// background-image: linear-gradient(to top right, #DD20B6, #833AD9);
