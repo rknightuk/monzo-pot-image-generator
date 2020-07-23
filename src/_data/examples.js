0b0331e0c3f020e765173341930ffda28dcdb33c
Robb Knight <desk@rknight.me>
2020-07-23 10:13:41 +0100

    Add example images

---
+++ src/_data/examples.js
@@ -0,0 +1,82 @@
+module.exports = [
+	{
+		icon: {
+			icon: "home",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#f1c40f",
+		image: null
+	},
+	{
+		icon: {
+			icon: "coins",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#e67e22",
+		image: null
+	},
+	{
+		icon: {
+			icon: "umbrella-beach",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#3498db",
+		image: null
+	},
+	{
+		icon: {
+			icon: "gifts",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#27ae60",
+		image: null
+	},
+	{
+		icon: {
+			icon: "playstation",
+			prefix: "fab",
+			size: 100
+		},
+		color: "#f7f7f7",
+		background: "#0070D1",
+		image: null
+	},
+	{
+		icon: {
+			icon: "xbox",
+			prefix: "fab",
+			size: 100
+		},
+		color: "#f7f7f7",
+		background: "#0F7C0F",
+		image: null
+	},
+	{
+		icon: {
+			icon: "steam",
+			prefix: "fab",
+			size: 100
+		},
+		color: "#f7f7f7",
+		background: "#333333",
+		image: null
+	},
+	{
+		icon: {
+			icon: "cc-amex",
+			prefix: "fab",
+			size: 100
+		},
+		color: "#f7f7f7",
+		background: "#006FCF",
+		image: null
+	}
+]
