4cb71773c4188211ddec28e935c3bf8278e03f54
Robb Knight <desk@rknight.me>
2020-07-26 22:28:09 +0100

    More examples

--- src/_data/examples.js
+++ src/_data/examples.js
@@ -31,12 +31,41 @@ module.exports = [
 	},
 	{
 		icon: {
+			icon: "gas-pump",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#9b59b6"
+	},
+	{
+		icon: {
+			icon: "cloud-rain",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#7f8c8d",
+		image: null
+	},
+	{
+		icon: {
 			icon: "gifts",
 			prefix: "fas",
 			size: 100,
 		},
 		color: "#f7f7f7",
 		background: "#27ae60",
+		image: null
+	},
+	{
+		icon: {
+			icon: "receipt",
+			prefix: "fas",
+			size: 100,
+		},
+		color: "#f7f7f7",
+		background: "#008D8E",
 		image: null
 	},
 	{
@@ -69,6 +98,16 @@ module.exports = [
 		background: "#333333",
 		image: null
 	},
+	{
+		icon: {
+			icon: 'dropbox',
+        	prefix: 'fab',
+        	size: 100,
+		},
+		color: '#f7f7f7',
+		background: '#007ee5',
+		image: null,
+    },
 	{
 		icon: {
 			icon: "cc-amex",
