886f842710aba0063837b2a03c6e91fdebe0ae63
Robb Knight <desk@rknight.me>
2021-09-04 20:06:41 +0100

    v2 live

--- src/assets/js/service-worker.js
+++ src/assets/js/service-worker.js
@@ -3,7 +3,7 @@ console.log('WORKER: executing.');
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'v1.2::';
+var version = 'v2::';
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
