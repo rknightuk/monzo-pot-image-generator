31d054c141de9352a9354c685a1cb5363d4e19e8
Dan Hughes <github@danhughes.dev>
2022-12-02 12:33:51 +0000

    Update brand colour palette

--- src/_data/colors.js
+++ src/_data/colors.js
@@ -1,58 +1,53 @@
 module.exports = {
-	background: [
-		"#FE4D59",
-		"#EC5B5C",
-		"#e67e22",
-		"#f1c40f",
-		"#3498db",
-		"#27ae60",
-		"#9b59b6",
-		"#34495e",
-		"#7f8c8d",
-		"#f7f7f7",
-		"#333333",
-		"#FAE54D",
-		"#70CF4B",
-		"#EB50AD",
-	],
-	icon: [
-		"#f7f7f7",
-		"#bdc3c7",
-		"#34495e",
-		"#333333"
-	],
-	gradients: [
-		{
-			left: "#1F3DF0",
-			right: "#3ABEDC"
-		},
-		{
-			left: "#1FB6DC",
-			right: "#3AD983"
-		},
-		{
-			left: "#982DF2",
-			right: "#5572E7"
-		},
-		{
-			left: "#3AF01E",
-			right: "#BFDC3A"
-		},
-		{
-			left: "#F29920",
-			right: "#E7553A",
-		},
-		{
-			left: "#DD20B6",
-			right: "#833AD9",
-		},
-		{
-			left: "#E8203B",
-			right: "#B73ABE"
-		},
-		{
-			left: "#F2D821",
-			right: "#E7813A"
-		}
-	]
-}
+  background: [
+    "#FF4D56",
+    "#E74C3C",
+    "#DB6F26",
+    "#F1C430",
+    "#2991CC",
+    "#61CD73",
+    "#6F48BC",
+    "#34495E",
+    "#707B8C",
+    "#E0E4E8",
+    "#14233C",
+    "#FAE54D",
+    "#70CF4B",
+    "#EB50AD",
+  ],
+  icon: ["#f7f7f7", "#bdc3c7", "#34495e", "#333333"],
+  gradients: [
+    {
+      left: "#1F3DF0",
+      right: "#3ABEDC",
+    },
+    {
+      left: "#1FB6DC",
+      right: "#3AD983",
+    },
+    {
+      left: "#982DF2",
+      right: "#5572E7",
+    },
+    {
+      left: "#3AF01E",
+      right: "#BFDC3A",
+    },
+    {
+      left: "#F29920",
+      right: "#E7553A",
+    },
+    {
+      left: "#DD20B6",
+      right: "#833AD9",
+    },
+    {
+      left: "#E8203B",
+      right: "#B73ABE",
+    },
+    {
+      left: "#F2D821",
+      right: "#E7813A",
+    },
+  ],
+};
